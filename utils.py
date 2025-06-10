85f1135561fb65f88a413ec0a50ce6a8bce3d68a
harsh17092006 <harsh.y1709@gmail.com>
2025-06-10 17:52:03 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+"""Utility functions for common tasks."""
+
+def is_positive(num: int) -> bool:
+    """Check if a number is positive."""
+    return num > 0
+
+def square(num: float) -> float:
+    """Return the square of a number."""
+    return num * num
