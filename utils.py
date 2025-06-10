193692f86ba3143c75c67ef3264f30c817500a54
harsh17092006 <harsh.y1709@gmail.com>
2025-06-10 18:01:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,6 @@ def is_positive(num: int) -> bool:
 def square(num: float) -> float:
     """Return the square of a number."""
     return num * num
+def reverse_string(s: str) -> str:
+    """Reverse the input string."""
+    return s[::-1]
